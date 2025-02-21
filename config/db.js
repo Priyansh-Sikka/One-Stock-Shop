83a6aa27c2a79e924c5fed2febb7fa4468c72ca6
Priyansh Sikka <135982932+Priyansh-Sikka@users.noreply.github.com>
2025-02-22 05:10:56 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectDB=async() =>{
+    try{
+        const mongoURL=process.env.MONGODB_URL;
+        const conn=await mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("Connected to the database");
+    } catch(error){
+        console.log("Database connection error:", error);
+    }
+}
+export default connectDB; 
